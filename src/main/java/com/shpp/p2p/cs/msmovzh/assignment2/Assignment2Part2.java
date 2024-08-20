943d10a38813deb40839c643633eb6ec14ab7d76
Bi4eps <smovzhmykyta@gmail.com>
2024-08-20 20:48:58 +0300

    feat: implement Assignment2Part2 - Draw 4 black circles in the corners and a white square in the center

---
+++ src/main/java/com/shpp/p2p/cs/msmovzh/assignment2/Assignment2Part2.java
@@ -0,0 +1,64 @@
+package com.shpp.p2p.cs.msmovzh.assignment2;
+
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import com.shpp.cs.a.graphics.WindowProgram;
+
+import java.awt.*;
+
+/**
+ * Draw 4 black circles in the corners and a white square in the center
+ */
+public class Assignment2Part2 extends WindowProgram {
+    private static final int APPLICATION_WIDTH = 600;
+    private static final int APPLICATION_HEIGHT = 600;
+    private static final int CIRCLES_DIAMETER = 200;
+    private static final int SQUARE_DIAMETER = 400;
+    private static final Color CIRCLES_COLOR = Color.BLACK;
+    private static final Color SQUARE_COLOR = Color.WHITE;
+
+    /**
+     * Draw 4 black circles in the corners and a white square in the center
+     */
+    public void run() {
+        setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+        printCircles();
+        printSquare();
+    }
+
+    /**
+     * Draw 4 black circles in the corners
+     */
+    private void printCircles() {
+        drawCircle(0, 0);
+        drawCircle(getWidth() - CIRCLES_DIAMETER, 0);
+        drawCircle(0, getHeight() - CIRCLES_DIAMETER);
+        drawCircle(getWidth() - CIRCLES_DIAMETER, getHeight() - CIRCLES_DIAMETER);
+    }
+
+    /**
+     * Draw white square in the center
+     */
+    private void printSquare() {
+        GRect square = new GRect((getWidth() - SQUARE_DIAMETER) / 2.0,
+                (getHeight() - SQUARE_DIAMETER) / 2.0,
+                SQUARE_DIAMETER, SQUARE_DIAMETER);
+        square.setColor(SQUARE_COLOR);
+        square.setFilled(true);
+        square.setFillColor(SQUARE_COLOR);
+        add(square);
+    }
+
+    /**
+     * Draw black circle
+     * @param x - the width of the upper left corner of the circle
+     * @param y - the height of the upper left corner of the circle
+     */
+    private void drawCircle(int x, int y) {
+        GOval circle = new GOval(x, y, CIRCLES_DIAMETER, CIRCLES_DIAMETER);
+        circle.setColor(CIRCLES_COLOR);
+        circle.setFilled(true);
+        circle.setFillColor(CIRCLES_COLOR);
+        add(circle);
+    }
+}
