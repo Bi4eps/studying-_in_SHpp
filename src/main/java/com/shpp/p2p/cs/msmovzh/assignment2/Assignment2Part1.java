4fe3e6c42734d8222c9b3bcdb1a298d568f30556
Bi4eps <smovzhmykyta@gmail.com>
2024-08-20 17:49:10 +0300

    feat: create package "assignment2" and class Assignment2Part1 inside it

---
+++ src/main/java/com/shpp/p2p/cs/msmovzh/assignment2/Assignment2Part1.java
@@ -0,0 +1,9 @@
+package com.shpp.p2p.cs.msmovzh.assignment2;
+
+import com.shpp.cs.a.console.TextProgram;
+
+public class Assignment2Part1 extends TextProgram {
+    public void run() {
+        println("Hello world!");
+    }
+}
